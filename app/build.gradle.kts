48b99757750b6db72d0e709b30403f91be3b29f6
w6 <w6@my1drive.by>
2026-08-17 15:25:30 +0300

    chore(version): bump version to 2.2.0 (versionCode 21) due to major feature additions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "by.w6.my1drive2"
         minSdk = 28
         targetSdk = 36
-        versionCode = 20
-        versionName = "2.1.8"
+        versionCode = 21
+        versionName = "2.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
