a72815bdb04a69d46db9a74237fce5fd488c5a90
w6 <w6@my1drive.by>
2026-09-09 00:41:42 +0300

    chore: bump version to 2.2.5 (27)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "by.w6.my1drive2"
         minSdk = 28
         targetSdk = 36
-        versionCode = 26
-        versionName = "2.2.4"
+        versionCode = 27
+        versionName = "2.2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
